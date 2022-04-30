522972e2476376348dd10a67616fea4825e217c3
Mona Chawla <Mona Chawla@MONA>
2022-04-30 22:01:22 +0530

    Add project files.

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MiddlewareAPI.Controllers
+{
+    [Route("country")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        [HttpGet("getAllCountries")]
+        public IEnumerable<Country> GetAllCountries()
+        {
+            return GetCountryDetails();
+        }
+
+        private List<Country> GetCountryDetails()
+        {
+            return new List<Country>()
+            {
+                new Country()
+                {
+                    CountryId = 1, CountryName = "India"
+                },
+                new Country()
+                {
+                    CountryId = 2, CountryName = "USA"
+                },
+                new Country()
+                {
+                    CountryId = 3, CountryName = "Germany"
+                }
+            };
+        }
+    }
+}
